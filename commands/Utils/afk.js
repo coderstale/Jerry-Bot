f8e2a7512716ce608036b3838dc26616d4171785
coderstale <85279322+coderstale@users.noreply.github.com>
2021-08-18 16:01:42 +0530

    Create afk.js

---
+++ commands/Utils/afk.js
@@ -0,0 +1,19 @@
+const Afk = require('../../models/afk-schema')
+
+module.exports = {
+    name: 'afk', 
+    aliases: 'af', 
+    async execute (client, message, args, cmd, Discord)  {    
+        let reason = args.join(" ");
+        if(!reason) reason = "The user is on AFK"; 
+        let afkProfile = await Afk.findOne({ userID: message.author.id}); 
+        if (!afkProfile) {
+            afkProfile = await new Afk({
+                userID: message.author.id, 
+                reason: reason, 
+            }); 
+            await afkProfile.save(); 
+            message.channel.send('You are now in AFK'); 
+        } else return message.channel.send('You are already in Afk bruh'); 
+    }
+}
