fc456b0eb082c8248284d599cf20d3684183dbad
coderstale <85279322+coderstale@users.noreply.github.com>
2021-08-18 16:49:12 +0530

    Update prefixmodel.js

--- models/prefixmodel.js
+++ models/prefixmodel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const guildSchema = new mongoose.Schema({
     guildID: String,
     lastEdited: String,  
-    prefix: { type: String, default: "crn!" },
+    prefix: { type: String, default: "j!" },
   });
   
   module.exports = new mongoose.model('Guild', guildSchema, 'guilds');
