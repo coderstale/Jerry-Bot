3c7e60a3e7a7c68c0a117b853b47cf134490e907
coderstale <85279322+coderstale@users.noreply.github.com>
2021-08-18 17:26:41 +0530

    Update afk-schema.js

--- models/afk-schema.js
+++ models/afk-schema.js
@@ -1,9 +1,20 @@
-const mongoose = require('mongoose'); 
+// const mongoose = require('mongoose'); 
 
-const afkSchema = new mongoose.Schema({
-    userID: String, 
-    reason: String, 
-    messageLeft: { type: Number, default: 1}
-}); 
+// const afkSchema = new mongoose.Schema({
+//     userID: String, 
+//     reason: String, 
+//     messageLeft: { type: Number, default: 1}
+// }); 
 
-module.exports = new mongoose.model("Afk", afkSchema, 'afks'); 
+// module.exports = new mongoose.model("Afk", afkSchema, 'afks'); 
+
+const mongoose = require('mongoose');
+
+let Schema = new mongoose.Schema({
+  Guild: String,
+  Member: String,
+  Content: String,
+  TimeAgo: String
+})
+
+module.exports = mongoose.model('afk', Schema)
