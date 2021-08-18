4212813b4a54033ff0e0adfbaa4ddbe3afd7097f
coderstale <85279322+coderstale@users.noreply.github.com>
2021-08-18 21:00:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.events = new Discord.Collection();
   require(`./handlers/${handler}`)(client, Discord);
 })
 
-mongoose.connect("mongodb+srv://coderstale:Thejoy@2929@jerry.emhgt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://coderstale:<password>@jerry.emhgt.mongodb.net/Data",{
   useUnifiedTopology: true,
   useNewUrlParser: true,
 }).then(()=>{
