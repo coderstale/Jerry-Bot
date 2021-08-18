f5fa1c86106400e2430c211c17c29ab92e03b7e6
coderstale <85279322+coderstale@users.noreply.github.com>
2021-08-18 16:57:59 +0530

    Delete prefixmodel.js

--- models/prefixmodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const guildSchema = new mongoose.Schema({
-    guildID: String,
-    lastEdited: String,  
-    prefix: { type: String, default: "j!" },
-  });
-  
-  module.exports = new mongoose.model('Guild', guildSchema, 'guilds');
