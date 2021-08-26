0479d3fbc5f5dc48b39e7c9b7ec2296aad508944
coderstale <85279322+coderstale@users.noreply.github.com>
2021-08-26 21:12:42 +0530

    Update invite.js

--- commands/General/invite.js
+++ commands/General/invite.js
@@ -8,7 +8,7 @@ module.exports = {
     .setColor("#e25800")
     .setTitle("Click here to Invite")
     .setURL("https://discord.com/api/oauth2/authorize?client_id=873412136578211840&permissions=8&scope=bot")
-    .setImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRevLaFkxzeLI8JQmOYdZYNToiXmmLahWWj4A&usqp=CAU')
+    .setImage('https://www.wallpapers13.com/wp-content/uploads/2016/11/Tom-and-Jerry-Tasty-sandwich-for-Tom-HD-Wallpaper-for-laptop-and-tablet-1920x1200-1366x768.jpg')
 
     message.channel.send(newEmbed);
   }
