ab64ceaca20ec63b939911ba9be64ae8a793ef3b
coderstale <85279322+coderstale@users.noreply.github.com>
2021-10-01 18:49:47 +0530

    Update invite.js

--- commands/General/invite.js
+++ commands/General/invite.js
@@ -5,7 +5,7 @@ module.exports = {
   execute(client, message, args, cmd, Discord) {
     message.delete()
     const newEmbed = new Discord.MessageEmbed()
-    .setColor("#e25800")
+    .setColor("#dc85e2")
     .setTitle("Click here to Invite")
     .setURL("https://discord.com/api/oauth2/authorize?client_id=893483868689162270&permissions=8&scope=bot")
     .setImage('https://www.wallpapers13.com/wp-content/uploads/2016/11/Tom-and-Jerry-Tasty-sandwich-for-Tom-HD-Wallpaper-for-laptop-and-tablet-1920x1200-1366x768.jpg')
