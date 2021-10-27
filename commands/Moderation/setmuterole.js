c07d7c22d30b961345314e54d52196f1ce30c636
coderstale <85279322+coderstale@users.noreply.github.com>
2021-10-27 14:51:27 +0530

    Update setmuterole.js

--- commands/Moderation/setmuterole.js
+++ commands/Moderation/setmuterole.js
@@ -6,7 +6,7 @@ module.exports = {
   async execute(client, message, args) {
     const role = await message.mentions.roles.first() || message.guild.roles.cache.get(args[0])
     if (!role) return message.channel.send(`Incorrect Usage.`)
-    if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("You don't have permission to use this command")
+    if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**You don't have permission to use this command.**")
     muteSchema.findOne({ Guild: message.guild.id }, async (err, data) => {
       if (err) throw err
       if (data) {
