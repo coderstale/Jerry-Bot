ee4422de11956013cae897622d7522871442b462
coderstale <85279322+coderstale@users.noreply.github.com>
2021-10-27 14:56:57 +0530

    Update lockdown.js

--- commands/Utils/lockdown.js
+++ commands/Utils/lockdown.js
@@ -6,7 +6,7 @@ module.exports = {
     message.delete()
     if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(new Discord.MessageEmbed().setDescription('**You do not have permissions to use this command!** <:Cookiechu:875018864403046450>').setColor('#e25800'));
     message.channel.updateOverwrite(message.channel.guild.roles.everyone, { VIEW_CHANNEL: false, SEND_MESSAGES: false });
-     message.channel.send(new Discord.MessageEmbed().setDescription('**This Channel is now under Lockdown!** <:Cookiechu:875018864403046450>').setColor('#e25800'));
+     message.channel.send(new Discord.MessageEmbed().setDescription('**This Channel is now under Lockdown!** <a:luvforkwiki:902844090117402625>').setColor('#dc85e2'));
   }
 }
 
