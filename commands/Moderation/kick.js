c0a2c89c42c68860426fb80001eb18dca66935b9
coderstale <85279322+coderstale@users.noreply.github.com>
2021-10-27 14:44:58 +0530

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -4,13 +4,13 @@ module.exports = {
   description: "This command kicks a member!",
   execute(client, message, args, cmd, Discord) {
     message.delete()
-    if (!message.mentions.users.first()) return message.channel.send(new Discord.MessageEmbed().setDescription('<:x_:876658908633137253> Too few arguments given.').setColor('#e25800').setAuthor(message.author.username).addFields(
+    if (!message.mentions.users.first()) return message.channel.send(new Discord.MessageEmbed().setDescription('<:x_:876658908633137253> Too few arguments given.').setColor('#dc85e2').setAuthor(message.author.username).addFields(
       { name: "**Usage:**", value: "`kick <user>`" },
     ));
     
     
     
-    if (!message.mentions.users.first().kickable) return message.channel.send(new Discord.MessageEmbed().setDescription('**Could not Kick this user!** <:Cookiechu:875018864403046450>').setColor('#e25800'));
+    if (!message.mentions.users.first().kickable) return message.channel.send(new Discord.MessageEmbed().setDescription('**Could not Kick this user!** <a:sedlyfe:902846910090579980>').setColor('#dc85e2'));
     
     
     
@@ -19,12 +19,12 @@ module.exports = {
     
     
     
-    if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(new Discord.MessageEmbed().setDescription('**You do not have permissions to kick a user!** <:Cookiechu:875018864403046450>').setColor('#e25800'));
+    if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(new Discord.MessageEmbed().setDescription('**You do not have permissions to kick a user!** <a:sedlyfe:902846910090579980>').setColor('#dc85e2'));
     const target = message.mentions.users.first();
     if (target) {
       const memberTarget = message.guild.members.cache.get(target.id);
       memberTarget.kick();
-      message.channel.send(new Discord.MessageEmbed().setDescription('**User has been kicked** <:SamiDev:875019906876317746>').setColor('#e25800'));
+      message.channel.send(new Discord.MessageEmbed().setDescription('**User has been kicked** <a:sedlyfe:902846910090579980>').setColor('#dc85e2'));
 
     }
   }
